f58ca1ac81ed41d676d5f056a588d539c51987f3
TiffanyNgoc <99504604+TiffanyNgoc@users.noreply.github.com>
2024-01-11 18:34:00 -0800

    Create Week2.1.cpp

---
+++ 1 C++ Basics: Selection and Iteration/Assignments/Week 2/Week2.1.cpp
@@ -0,0 +1,40 @@
+// Complete the code on the left such that the code returns 17. Please note there are two types of errors present in the code:
+// A compilation error - the compiler sees a problem and throws an error to alert you.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //fix the code below this line
+
+  int a = 5;
+  int b = 9;
+
+  //fix the code above this line
+  
+  cout << (a * 3 + b - 8 / 2) << endl;
+  
+  return 0;
+  
+}
+
+// A run-time error - an error when the code is running.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //fix the code below this line
+
+  int a = 4;
+  int b = 9;
+
+  //fix the code above this line
+  
+  cout << (a * 3 + b - 8 / 2) << endl;
+  
+  return 0;
+  
+}
