95a2ad83fc8b63f4106e4be9a0ae1cb3df6a6f07
TiffanyNgoc <99504604+TiffanyNgoc@users.noreply.github.com>
2024-01-11 18:34:47 -0800

    Create Week2.2.cpp

---
+++ 1 C++ Basics: Selection and Iteration/Assignments/Week 2/Week2.2.cpp
@@ -0,0 +1,16 @@
+// Using ONLY numbers and operators, print an expression that evaluates to false. Make sure to use cout << boolalpha << in your code. Otherwise, the system will print 0 or 1 instead of false or true.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+
+  cout << boolalpha << (5 > 10) << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
