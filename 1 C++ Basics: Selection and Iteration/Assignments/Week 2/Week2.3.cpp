fedae8a91cb8c85fec894bed8521e1fe554bdf9b
TiffanyNgoc <99504604+TiffanyNgoc@users.noreply.github.com>
2024-01-11 18:35:15 -0800

    Create Week2.3.cpp

---
+++ 1 C++ Basics: Selection and Iteration/Assignments/Week 2/Week2.3.cpp
@@ -0,0 +1,21 @@
+// Write a program that outputs Hello world. Your program must do the following things:
+// Use two variables, one assigned to Hello and the other assigned to world.
+// Use just one cout << statement.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+
+string hello = "Hello";
+string world = "world";
+
+cout << hello << " " << world << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
