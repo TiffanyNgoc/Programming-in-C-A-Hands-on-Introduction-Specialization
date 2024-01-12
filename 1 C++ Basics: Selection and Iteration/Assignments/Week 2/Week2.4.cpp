c2708167ce955d9da3be7879aa59138389ea56fb
TiffanyNgoc <99504604+TiffanyNgoc@users.noreply.github.com>
2024-01-11 18:35:42 -0800

    Create Week2.4.cpp

---
+++ 1 C++ Basics: Selection and Iteration/Assignments/Week 2/Week2.4.cpp
@@ -0,0 +1,22 @@
+// Write a program that divides 7 by 2 and prints out the result (3.5) without using the printf() command. Your program must do the following things:
+// Declare two variables and assign them to the appropriate values, and use them to calculate your quotient of 3.5.
+// Include just one division (/) operator in your code.
+// Use just one cout << statement.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //add code below this line
+
+int num = 7;
+int denom = 2;
+
+cout << (double)num / denom << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
