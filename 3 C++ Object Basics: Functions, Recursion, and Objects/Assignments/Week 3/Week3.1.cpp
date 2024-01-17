d46e525eba0d3f7545b21f31471ce81e4faa83e2
TiffanyNgoc <99504604+TiffanyNgoc@users.noreply.github.com>
2024-01-17 15:39:27 -0800

    Update Week3.1.cpp

--- 3 C++ Object Basics: Functions, Recursion, and Objects/Assignments/Week 3/Week3.1.cpp
+++ 3 C++ Object Basics: Functions, Recursion, and Objects/Assignments/Week 3/Week3.1.cpp
@@ -1,1 +1,31 @@
-// 
+// Define the class PracticeClass. It should have only a single string attribute name. This class should not have a constructor.
+// Expected Result:
+Class Object
+// DO NOT CHANGE the existing code in main.
+PracticeClass obj;
+obj.name = "Class Object";
+cout << obj.name;
+
+// Solved Code
+#include <iostream>
+using namespace std;
+
+//add class definitions below this line
+    
+class PracticeClass {
+public:
+    string name;
+};
+
+  
+//add class definitions above this line
+
+int main() {
+  
+  PracticeClass obj;
+  obj.name = "Class Object";
+  cout << obj.name;
+  
+  return 0;
+  
+}
